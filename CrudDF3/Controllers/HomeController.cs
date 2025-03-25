a84863adeef839c16dc68784bac8afffe2636f55
Estiven <jhonestiven059@gmail.com>
2025-03-25 15:10:14 -0500

    login diferente

--- CrudDF3/Controllers/HomeController.cs
+++ CrudDF3/Controllers/HomeController.cs
@@ -4,13 +4,6 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        ViewData["MostrarNav"] = false; // Cambiar a true si el usuario está autenticado
-        return View();
-    }
-
-    public IActionResult Dashboard()
-    {
-        ViewData["MostrarNav"] = true; // Mostrar el nav en el dashboard
-        return View();
+        return View(); // MUESTRA EL HOME SIEMPRE
     }
 }
